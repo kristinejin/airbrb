dbff1b10334a766e46fa96815e0a39738269acd9
YITONG JIN <z5362038@ad.unsw.edu.au>
2022-10-26 10:24:47 +1100

    register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Register from './pages/register.jsx';
+
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register/>}></Route>
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
