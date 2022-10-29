10bde876025c2a91f18c4084342f2ac97f2a4b2a
unknown <z5316653@ad.unsw.edu.au>
2022-10-29 11:25:06 +1100

    forgot to save

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 
 function App() {
-<<<<<<< HEAD
   return (
     <BrowserRouter>
       <Router/>
     </BrowserRouter>
     
   );
-=======
-  return <></>;
->>>>>>> update-b6d71d8f9a4ef44fb41ebe12dbd499a135894897
 }
 
 export default App;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-//import reportWebVitals from './reportWebVitals';
-=======
->>>>>>> update-b6d71d8f9a4ef44fb41ebe12dbd499a135894897
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,11 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-<<<<<<< HEAD
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
-=======
->>>>>>> update-b6d71d8f9a4ef44fb41ebe12dbd499a135894897
