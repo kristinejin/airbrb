2a8ad90310ff5ce5d5d65904d4ccb0afac6f10a3
YITONG JIN <z5362038@ad.unsw.edu.au>
2022-11-10 13:19:29 +1100

    changed login password field to password

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,65 +1,84 @@
-import React from 'react'
+import React from "react";
 
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-import { withStyles } from '@mui/styles';
-import { apiCall } from '../util/api';
-import { useNavigate } from 'react-router-dom';
+import { withStyles } from "@mui/styles";
+import { apiCall } from "../util/api";
+import { useNavigate } from "react-router-dom";
 
-const styles = theme => ({
+const styles = (theme) => ({
     main: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 'auto',
-        height: 'auto',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "auto",
+        height: "auto",
     },
     loginBox: {
-        marginTop: '20px',
-        display:'flex',
-        flexDirection: 'column',
-        width: '400px',
-        gap: '10px',
+        marginTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        width: "400px",
+        gap: "10px",
     },
-    loginbuttons: {
-
-    }
-})
+    loginbuttons: {},
+});
 
 const Login = (props) => {
-    const [userEmail, setUserEmail] = React.useState('');
-    const [userPassword, setUserPassword] = React.useState('');
+    const [userEmail, setUserEmail] = React.useState("");
+    const [userPassword, setUserPassword] = React.useState("");
     const signIn = (email, password) => {
-        apiCall('user/auth/login', 'POST', { "email": email, "password": password })
-            .then((data) => {
-                console.log(data.token);
-                localStorage.setItem("token", data.token);
-                localStorage.setItem("email", email);
-                window.location.href="/hostedlistings";
-            })
-    }
+        apiCall("user/auth/login", "POST", {
+            email: email,
+            password: password,
+        }).then((data) => {
+            console.log(data.token);
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("email", email);
+            window.location.href = "/hostedlistings";
+        });
+    };
 
     const navigate = useNavigate();
-    const {classes} = props;
+    const { classes } = props;
     return (
         <main className={classes.main}>
             <Box className={classes.loginBox}>
                 <Typography component="h1" variant="h5">
                     Sign in to AirBNB
                 </Typography>
-                <TextField fullWidth className={classes.loginfield} id="email" label="Email" value={userEmail} onChange={e => setUserEmail(e.target.value)}></TextField>
-                <TextField fullWidth className={classes.loginfield} id="password" label="Password" value={userPassword} onChange={e => setUserPassword(e.target.value)}></TextField>
+                <TextField
+                    fullWidth
+                    className={classes.loginfield}
+                    id="email"
+                    label="Email"
+                    value={userEmail}
+                    onChange={(e) => setUserEmail(e.target.value)}
+                ></TextField>
+                <TextField
+                    fullWidth
+                    className={classes.loginfield}
+                    id="password"
+                    label="Password"
+                    type="password"
+                    value={userPassword}
+                    onChange={(e) => setUserPassword(e.target.value)}
+                ></TextField>
 
                 <Box className={classes.loginbuttons}>
-                    <Button onClick={() => signIn(userEmail, userPassword)}>Sign in</Button>
-                    <Button onClick={() => navigate('/register')}>Register</Button>
+                    <Button onClick={() => signIn(userEmail, userPassword)}>
+                        Sign in
+                    </Button>
+                    <Button onClick={() => navigate("/register")}>
+                        Register
+                    </Button>
                 </Box>
             </Box>
         </main>
-    )
-}
+    );
+};
 
 export default withStyles(styles)(Login);
