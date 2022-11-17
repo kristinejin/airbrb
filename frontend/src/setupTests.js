6baf2694b8d7a8f581f17877f344c44c8a133965
unknown <z5316653@ad.unsw.edu.au>
2022-11-17 21:42:11 +1100

    using enzyme 17

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { configure } from 'enzyme';
-import Adaptor from 'enzyme-adapter-react-16';
-configure({ adapter: new Adaptor() });
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
